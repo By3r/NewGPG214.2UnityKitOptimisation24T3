458e3fcc5c32b448496dd875b14bf57f2ee55269
Dana <tetekim31@gmail.com>
2024-12-22 16:22:48 +0400

    Updated coin collection logic with a new analytic event

--- Assets/GPG214.2DanaTaha24T3/DanasSscripts/GeneralGame/CoinCollection.cs
+++ Assets/GPG214.2DanaTaha24T3/DanasSscripts/GeneralGame/CoinCollection.cs
@@ -1,27 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
+using Unity.Services.Analytics;
+using Unity.Services.Core;
 using UnityEngine;
 
 namespace Dana
 {
     /// <summary>
-    /// Simple coin collecton script. Everytime the Player collides with the coin gameobject. 
+    /// Simple coin collection script. Every time the Player collides with the coin GameObject, it records an analytics event.
     /// </summary>
-    
+  
     public class CoinCollection : MonoBehaviour
     {
         #region Variables
         [SerializeField] private int coinValue = 1; 
         #endregion
 
+        private IEnumerator Start()
+        {
+            yield return UnityServices.InitializeAsync();
+            AnalyticsService.Instance.StartDataCollection();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                Debug.Log("Coin collected!");
-                // Update collected coins amount of the player.
-                // Make the player's data update the amount && I'll then test out account switch to see if there is a diffference in stored data.
-                // Try to make it trackable through analytics.
+                if (PlayerDataManager.instance != null)
+                {
+                    PlayerDataManager.instance.AddCoins(coinValue);
+                }
+
+                CustomEvent coinCollectionEvent = new CustomEvent("CoinCollectionEvent")
+                {
+                    { "coinValue", coinValue },
+                    { "coinPositionX", transform.position.x },
+                    { "coinPositionY", transform.position.y },
+                    { "coinPositionZ", transform.position.z },
+                };
+
+                AnalyticsService.Instance.RecordEvent(coinCollectionEvent);
                 Destroy(gameObject);
             }
         }
