b7842e539e8019a005b56388843a3268bbffe085
Dana <tetekim31@gmail.com>
2024-12-22 08:27:24 +0400

    This the asset bundle logic which i forgot to commit earlier

--- Assets/GPG214.2DanaTaha24T3/DanasSscripts/GeneralGame/AssetBundleLoaderAndDownloader.cs
+++ Assets/GPG214.2DanaTaha24T3/DanasSscripts/GeneralGame/AssetBundleLoaderAndDownloader.cs
@@ -8,45 +8,87 @@ namespace Dana
     public class AssetBundleLoaderAndDownloader : MonoBehaviour
     {
         #region Variables
+        [Header("JSON URL")]
+        [Tooltip("URL of the JSON file containing asset bundle data.")]
+        [SerializeField] private string jsonFileURL = "https://drive.google.com/uc?id=1wi--oQIJSNw4uETU6V7F8w8qML72dyxK";
+
         [Header("Asset Bundle Info")]
-        [Tooltip("Place the google drive download link for the asset bundle.")]
-        [SerializeField] private string assetBundleUrl = "https://drive.google.com/uc?id=1654VnkT7Php08TF4HMzdNQgV6otI7iP3";
+        [Tooltip("Write the name of a base asset bundle file, the name will change eventually through the JSON file.")]
+        [SerializeField] private string bundleName = "basiclevelassets_19.12.24.1";
 
-        [Tooltip("Write the name of the asset bundle file.")]
-        [SerializeField] private string bundleName = "basiclevelassets_19.12.24";
-
-        [Header("Script references")]
+        [Header("Script References")]
         [Tooltip("Reference to the prefab spawner based on texture read script.")]
         [SerializeField] private PrefabSpawnerBasedOnTextureRead prefabSpawnerScript;
 
         private string _localBundlePath;
+        private AssetBundleJSONData _jsonData;
         #endregion
 
         private void Start()
         {
             SetLocalPathForAssetBundle(Application.persistentDataPath, bundleName);
-            StartCoroutine(DownloadAndLoadAssetBundle());
+            StartCoroutine(CheckAndDownloadBundle());
         }
 
         #region Private Functions
         /// <summary>
+        /// Fetch metadata, compare versions, and download the new bundle if necessary.
+        /// </summary>
+        private IEnumerator CheckAndDownloadBundle()
+        {
+            UnityWebRequest metadataRequest = UnityWebRequest.Get(jsonFileURL);
+            yield return metadataRequest.SendWebRequest();
+
+            if (metadataRequest.result != UnityWebRequest.Result.Success)
+            {
+                yield break;
+            }
+
+            string json = metadataRequest.downloadHandler.text;
+            _jsonData = JsonUtility.FromJson<AssetBundleJSONData>(json);
+
+            if (_jsonData == null)
+            {
+                yield break;
+            }
+
+            if (_jsonData.newBundle.name != bundleName)
+            {
+                bundleName = _jsonData.newBundle.name;
+                SetLocalPathForAssetBundle(Application.persistentDataPath, bundleName);
+
+                yield return DownloadAndLoadAssetBundle(_jsonData.newBundle.url);
+            }
+            else
+            {
+                yield return LoadBundleFromDownloadedFile();
+            }
+        }
+
+        /// <summary>
         /// Download the asset bundle and load it.
         /// </summary>
-        private IEnumerator DownloadAndLoadAssetBundle()
+        private IEnumerator DownloadAndLoadAssetBundle(string url)
         {
-            Debug.Log($"Downloading the asset bundle from {assetBundleUrl}");
 
-            UnityWebRequest bundleRequest = UnityWebRequest.Get(assetBundleUrl);
+            UnityWebRequest bundleRequest = UnityWebRequest.Get(url);
             bundleRequest.downloadHandler = new DownloadHandlerFile(_localBundlePath);
 
             yield return bundleRequest.SendWebRequest();
 
             if (bundleRequest.result != UnityWebRequest.Result.Success)
             {
-                Debug.LogError($"Failed to download asset bundle: {bundleRequest.error}");
                 yield break;
             }
 
+            yield return LoadBundleFromDownloadedFile();
+        }
+
+        /// <summary>
+        /// Loads the asset bundle from the local file.
+        /// </summary>
+        private IEnumerator LoadBundleFromDownloadedFile()
+        {
             AssetBundle bundle = AssetBundle.LoadFromFile(_localBundlePath);
             if (bundle != null)
             {
@@ -57,13 +99,15 @@ namespace Dana
                 }
                 else
                 {
-                    Debug.LogError("Make sure the script is assigned!!");
+                    //   Debug.LogError("Prefab spawner script is not assigned.");
                 }
             }
             else
             {
-                Debug.LogError("Could not load asset bundle...");
+                //    Debug.LogError("Failed to load the asset bundle. The file may be corrupted.");
             }
+
+            yield return null;
         }
 
         /// <summary>
@@ -77,4 +121,23 @@ namespace Dana
         }
         #endregion
     }
+
+    #region Json for parsing
+    /// <summary>
+    /// Json struct for JSON parsing.
+    /// </summary>
+    [System.Serializable]
+    public class AssetBundleJSONData
+    {
+        public BundleInfo currentBundle;
+        public BundleInfo newBundle;
+    }
+
+    [System.Serializable]
+    public class BundleInfo
+    {
+        public string name;
+        public string url;
+    }
+    #endregion
 }
